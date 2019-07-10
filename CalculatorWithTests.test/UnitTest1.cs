d1665e0e57635ffe0baedd25c76dbd1ae3db4a31
Tanner <tanner.drayton01@gmail.com>
2019-07-10 10:02:27 -0500

    Added shouldSubtract test

--- CalculatorWithTests.test/UnitTest1.cs
+++ CalculatorWithTests.test/UnitTest1.cs
@@ -26,7 +26,25 @@ namespace CalculatorWithTests.test
             Assert.Equal(expected, actual);
         }
 
-        
+        [Theory]
+        [InlineData(3, 2, 1)]
+        [InlineData(72, 12, 60)]
+        [InlineData(-99, -33, -66)]
+        [InlineData(-10, 100, -110)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-32, -16, -16)]
+        [InlineData(15, -15, 30)]
+        public void ShouldSubtract(decimal num3, decimal num4, decimal expected)
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            decimal actual = Calculator.Subtract(num3, num4);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
 
     }
 }
